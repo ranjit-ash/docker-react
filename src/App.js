7012955d4e44f8881a663c1e167f8dc1d8692e65
ranjit-ash <ranjit.ash@gmail.com>
2021-02-09 10:51:08 +0530

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          This is my first docker project! with docker compose
+          This is my first docker project! with docker compose!
         </p>
         <a
           className="App-link"
